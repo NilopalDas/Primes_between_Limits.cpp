7105a158f0850f32684f36496eab3aa4fe41c84d
NilopalDas <65865676+NilopalDas@users.noreply.github.com>
2020-05-25 11:45:40 +0530

    Add files via upload

---
+++ Shutdown program.cpp
@@ -0,0 +1,17 @@
+/*Shutdown Program in C++*/
+
+#include<iostream>
+#include<stdlib.h>
+
+/*Some changes are to be introduced*/
+
+using namespace std;
+
+int main()
+{
+	system("color 1a");
+	system("shutdown /s");   //to instamtly shutdown the pc, write '/p' instead of '/s' and compile the file...
+	return 0;
+}
+
+//Thank You!
